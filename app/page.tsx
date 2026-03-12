c467760edccf123ef6cb37300b17117dc8c9d6b8
Leo <sokolov.nikita01@list.ru>
2026-03-13 02:47:19 +0300

    + шапка

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,66 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Avatar, AvatarFallback, AvatarImage, AvatarBadge } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
+import { BadgeInfo, Settings  } from 'lucide-react';
+import Image from "next/image";
+
 import { Button } from "@/components/ui/button"
+import { ButtonGroup } from "@/components/ui/button-group"
 
 export default function Page() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+    
+    <div className="flex justify-center pt-5">
+      <Card className="w-full max-w-7xl shadow p-8">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-8">
+            <Avatar className="shadow size-25">
+              <AvatarImage src="https://avatars.githubusercontent.com/u/170928669?v=4"/>
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="grid content-between gap-1">
+              <div className="flex gap-2 ">
+                <CardTitle className="">
+                  Соколов Никита
+                </CardTitle>
+                <CardDescription >
+                  @Leomagnuss
+                </CardDescription>
+                <Badge className="bg-green-100 text-green-700 dark:bg-green-950 dark:text-green-300">
+                  Online
+                </Badge>
+              </div>
+              <div>
+                  <Badge variant="outline"><BadgeInfo />Специалист поддержки</Badge>
+              </div>
+              <CardDescription>Сотрудник поддержки L2, качественная поддержка и администрирование сервисов, диагностика и обслуживание. </CardDescription>
+            </div>
+            <div className="flex flex-col items-start gap-8">
+            <ButtonGroup>
+              <Button variant="outline" className="cursor-pointer" size="sm">
+                Редактировать профиль
+              </Button>
+              <Button variant="outline" className="cursor-pointer" size="sm">
+                Поделиться
+              </Button>
+              <Button variant="outline" className="cursor-pointer" size="sm">
+                Настройки <Settings />
+              </Button>
+            </ButtonGroup>
+            
+            </div>
+          </div>
         </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+      </Card>
     </div>
-  )
+
+  );
 }
